f65fa8567a46c4f1c48f77d54aa7c2d8a8125e20
unknown <raquelgoulart@gmail.com>
2024-03-26 22:38:08 +0000

    update jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 // react, components, estáticos
-
+import { useContext, useEffect } from 'react';
+import { QuizContext } from './context/quiz';
 import Welcome from './components/Welcome';
+import Question from './components/Question';
+
 import './App.css';
 
 
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
   
+  useEffect(() => {
+    dispatch({type: "REORDER_QUESTIONS"})
+  }, [])
 
   return (
     <div className="App">
         <h1>Quiz de Programação</h1>
-        <Welcome />
+        {quizState.gameStage === "Start" && <Welcome />}
+        {quizState.gameStage === "Playing" && <Question />}
+        
     </div>
   )
 }
